9f4f74791c91d61ae20bb6766f6c7822cd122489
Nana Kwakye Owusu <dkwakye94@gmail.com>
2023-04-03 06:12:39 -0700

    ALL TASKS

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,15 +6,15 @@
 class Rectangle:
     """Represent a rectangle.
     Attributes:
-    number_of_instances (int): The number of Rectangle instances.
+        number_of_instances (int): The number of Rectangle instances.
     """
 
     number_of_instances = 0
 
-    def __init__(self, width=0,height=0):
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
-        width (int): The width of the new rectangle.
+            width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
         type(self).number_of_instances += 1
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -5,18 +5,18 @@
 
 class Rectangle:
     """Represent a rectangle.
-    Atributes:
-    number_of_instances (int): The number of Rectangle instances.
-    print_symbol (any): The symbol used for string representation.
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
     """
 
     number_of_instances = 0
     print_symbol = "#"
 
-    def __init__(self, width-0, height=0):
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
-         width (int): The width of the new rectangle.
+            width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
         type(self).number_of_instances += 1
