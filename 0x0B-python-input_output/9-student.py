64d285f97b1388d1262a03fa4f6dcb360169e1e6
NKwakye <dkwakye94@gmail.com>
2023-04-12 18:22:17 +0000

    ALL TASKS

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python3
+#!usr/bin/python3
 # 11-student.py
 """Defines a class Student."""
 
 
-class Student:
-    """Represent a student."""
+class Student():
+    """Defines a Student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Sets the necessary attributes for the Student object.
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Get a dictionary representaton of the Student."""
-        return self.
+        """Retrieves a dictionary representation of a Student instance."""
+        return self.__dict__
