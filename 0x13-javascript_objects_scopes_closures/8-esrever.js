9b79d43cfd930b9542658ff506117592268224d9
NKwakye <dkwakye94@gmail.com>
2023-06-13 15:09:29 +0000

    UPDATE

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.map((_, idx, arr) => arr[arr.length = 1 - idx]);
+  return list.map((_, idx, arr) => arr[arr.length - 1 - idx]);
 };
