a61d697d433b57d7eda77c16e6fca33eb7f763ec
NKwakye <dkwakye94@gmail.com>
2023-07-25 20:55:33 +0000

    ADVANCED

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,22 @@
 #!/usr/bin/node
+/*  computes the number of tasks completed by user id. */
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  else {
+    const resp = {};
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (resp[json[i].userId] === undefined) {
+          resp[json[i].userId] = 0;
+        }
+        resp[json[i].userId]++;
       }
-    });
-    console.log(completed);
+    }
+    console.log(resp);
   }
-})
+});
